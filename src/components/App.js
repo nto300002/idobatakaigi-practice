c1bbde4cac833f4b790b22fcaf0c302497e3e744
nto300002 <samonkntd@gmail.co>
2021-10-03 21:22:12 +0900

    save name in japanese

--- src/components/App.js
+++ src/components/App.js
@@ -1,12 +1,19 @@
 import React, { useState } from 'react';
+import Main from "./Main"
 
 import SignIn from "./SignIn";
 
 export default () => {
-const [name, setName] = useState('');
-console.log({ name });
+  const [name, setName] = useState('');
+  console.log({ name });
 
-  return (
-    <SignIn setName={setName}/>
-  );
+  if(name === ""){
+    return (
+      <SignIn setName={setName} />
+    );
+  }else{
+    return <Main name={name} />
+  }
+
+
 };
