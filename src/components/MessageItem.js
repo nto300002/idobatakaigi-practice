d143baf848b588735813c693d75748fd7f1d7185
nto300002 <samonkntd@gmail.co>
2021-10-05 07:55:04 +0900

    display-new-message

--- src/components/MessageItem.js
+++ src/components/MessageItem.js
@@ -1,42 +1,52 @@
-import React from 'react'
-import { ListItem,ListItemText,Avatar,ListItemAvatar,Typography } from '@material-ui/core';
-import { makeStyles } from "@material-ui/core/styles";
-import { gravatarPath } from "../gravatar"
+import React, { useEffect, useRef } from 'react';
+import {
+  Avatar,
+  ListItem,
+  ListItemAvatar,
+  ListItemText,
+  Typography,
+} from '@material-ui/core';
+import { makeStyles } from '@material-ui/core/styles';
 
-const useStyles = makeStyles((theme) => ({
-  root: {
-    width: "100%",
-    maxWidth: "36ch",
-    backgroundColor: theme.palette.background.paper,
-  },
+import { gravatarPath } from '../gravatar';
+
+const useStyles = makeStyles(() => ({
   inline: {
-    display: "inline",
+    display: 'inline',
   },
 }));
 
-const MessageItem = ({name, text}) => {
+const MessageItem = ({ isLastItem, name, text }) => {
+  const ref = useRef(null);
   const classes = useStyles();
   const avatarPath = gravatarPath(name);
-  return (
-      <ListItem divider={true}>
-        <ListItemAvatar>
-          <Avatar  src={avatarPath} />
-        </ListItemAvatar>
-        <ListItemText
-          primary={name}
-          secondary={  
-              <Typography
-                sx={{ display: 'inline' }}
-                component="span"
-                variant="body2"
-                color="text.primary"
-              >
-                {text}
-              </Typography>
-          }
-        />
-      </ListItem>
-  )
-}
 
-export default MessageItem
+  useEffect(() => {
+    if (isLastItem) {
+      ref.current.scrollIntoView({ behavior: 'smooth' });
+    }
+  }, [isLastItem]);
+
+  return (
+    <ListItem divider={true} ref={ref}>
+      <ListItemAvatar>
+        <Avatar src={avatarPath} />
+      </ListItemAvatar>
+      <ListItemText
+        primary={name}
+        secondary={
+          <Typography
+            component="span"
+            variant="body2"
+            className={classes.inline}
+            color="textPrimary"
+          >
+            {text}
+          </Typography>
+        }
+      />
+    </ListItem>
+  );
+};
+
+export default MessageItem;
